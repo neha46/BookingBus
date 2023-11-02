a584839d7d2882d782a6562f415d5d593174ceb3
neha46 <nehamishra6714@gmail.com>
2023-11-02 18:47:17 +0530

    user(display all buses)

---
+++ client/src/components/Bus.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Bus = ({bus}) => {
+    const navigate=useNavigate()
+  return (
+    <div className='card p-3 gap-1'>
+      <h1 className='text-lg'>{bus.name}</h1>
+      <hr/>
+      <div className="d-flex justify-content-between">
+       <div>
+        <p className='text-sm'>From</p>
+        <p className='text-sm'> {bus.from}</p>
+       </div>
+       <div>
+        <p className='text-sm'>To</p>
+        <p className='text-sm'> {bus.to}</p>
+       </div>
+       <div>
+        <p className='text-sm'>Fare</p>
+        <p className='text-sm'>$ {bus.fare} /-</p>
+       </div>
+      </div>
+
+      <div className="d-flex justify-content-between">
+       <div>
+        <p className='text-sm'>JourneyDate</p>
+        <p className='text-sm'> {bus.journeyDate}</p>
+       </div>
+       <h1 className='text-lg underline' onClick={()=>{
+        navigate(`/book-now/${bus._id}`)
+       }}>BookNow</h1>
+       </div>
+    </div>
+  );
+}
+
+export default Bus;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { axiosInstance } from '../helper/axiosInstance.js';
+import { Col,Row, message } from 'antd';
+import { ShowLoading,HideLoading } from '../redux/alertSlice.js';
+import Bus from '../components/Bus.js';
+
 
 const Home = () => {
+  const [showBusForm,setShowBusForm]=useState(false);
+    const[buses,setBuses]=useState([]);
   const {user}=useSelector((state)=>state.users)
+   const dispatch=useDispatch();
+
+  const getBuses=async()=>{
+ 
+    try{
+      
+            dispatch(ShowLoading());
+            const response=await axiosInstance.post('/api/buses/get-all-buses',{})
+            console.log(response.data.data)
+
+            
+            if(response.data.success){
+                message.success(response.data.message)
+                setBuses(response.data.data)
+              }
+             
+            else{
+                message.error(response.data.message)
+            }
+              dispatch(HideLoading())
+               
+        } 
+    catch(error){
+       dispatch(HideLoading())
+      message.error(error.message)
+    }
+  }
+  console.log("buses is :",buses)
+  useEffect(()=>{
+    getBuses();
+  },[])
+
   return (
     <div>
-      <h1> Welcome to Home page</h1>
-      <h2>{user.name}</h2>
-      <h2>{user.email}</h2>
+    <div>
+      <h1> Welcome {user.name} at Home page</h1>
+      
+    </div>
+    <div>
+      <Row>
+        {buses.map(bus=>
+         <Col lg={12} xs={24} sm={24}><Bus bus={bus}/></Col>
+        )}
+      </Row>
+    </div>
     </div>
   );
 }
